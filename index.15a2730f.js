e41c4a29e2b7bd56af72a53098e1b6dd5ff83fad
Yurii <urabelivr@gmail.com>
2024-06-19 14:34:15 +0200

    make build

---
+++ index.15a2730f.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");t.style.top="".concat(e.clientHeight/2-t.clientHeight/2,"px"),t.style.left="".concat(e.clientWidth/2-t.clientWidth/2,"px");
+//# sourceMappingURL=index.15a2730f.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,7 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+spider.style.top = `${wall.clientHeight / 2 - spider.clientHeight / 2}px`;
+spider.style.left = `${wall.clientWidth / 2 - spider.clientWidth / 2}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
